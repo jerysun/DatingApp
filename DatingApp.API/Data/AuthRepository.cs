7042ecceeedc02119293de3956e4d7a75ff4e570
Jerry Sun <Jerry Sun>
2019-08-24 17:30:47 +0200

    create User table using ef, add some Authentication related codes

---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,47 @@
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext context;
+        public AuthRepository(DataContext context)
+        {
+            this.context = context;
+
+        }
+        public Task<User> Login(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await context.Users.AddAsync(user);//write to the memory
+            await context.SaveChangesAsync();  //persist it to the DB
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password)));
+            }
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
