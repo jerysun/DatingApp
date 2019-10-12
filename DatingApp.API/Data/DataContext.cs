61e2424d1c26239084a1c1b308355f2d1d7a9395
Jerry Sun <Jerry Sun>
2019-10-12 02:06:25 +0200

    Create table Likes, many2many relationship

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -12,5 +12,31 @@ namespace DatingApp.API.Data
         public DbSet<User> Users { get; set; }
 
         public DbSet<Photo> Photos { get; set; }
+        public DbSet<Like> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // k means combination(actually k is an instance of class Like)
+            // which helps HasKey() form the primary key, so table Likes
+            // will have a primary key although we don't create this pro-
+            // perty explicitly in Like.cs.
+            // builder.Entity<T>() returns an object that can be used to
+            // configure a given entity type in the model. If the entity
+            // type is not already part of the model, it will be added to
+            // the model.
+            builder.Entity<Like>().HasKey(k => new {k.LikerId, k.LikeeId});
+
+            builder.Entity<Like>()
+                .HasOne(u => u.Likee)
+                .WithMany(u => u.Likers)
+                .HasForeignKey(u => u.LikeeId)     // generate FK1(Foreign Key)
+                .OnDelete(DeleteBehavior.Restrict);// Rule on FK1
+            
+            builder.Entity<Like>()
+                .HasOne(u => u.Liker)
+                .WithMany(u => u.Likees)
+                .HasForeignKey(u => u.LikerId)     // generate FK2(Foreign Key)
+                .OnDelete(DeleteBehavior.Restrict);// Rule on FK2, yes this table has 2 FKs!
+        }
     }
 }
